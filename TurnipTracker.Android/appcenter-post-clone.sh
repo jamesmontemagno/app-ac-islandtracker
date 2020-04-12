ef0c1935f9d7365b62ac2d05a672ccf102629c3c
James Montemagno <james.montemagno@gmail.com>
2020-04-11 21:35:10 -0700

    Print files

--- TurnipTracker.Android/appcenter-post-clone.sh
+++ TurnipTracker.Android/appcenter-post-clone.sh
@@ -5,4 +5,6 @@ echo "Variables:"
 # Updating manifest
 sed -i '' "s/AC_ANDROID/$AC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
 
+cat $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+
 echo "Manifest updated!"
--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -8,4 +8,9 @@ sed -i '' "s/AC_IOS/$AC_IOS/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xam
 
 sed -i '' "s/APP_SECRET/$APP_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker.iOS/Info.plist
 
+
+cat $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+
+$BUILD_REPOSITORY_LOCALPATH/TurnipTracker.iOS/Info.plist
+
 echo "Manifest updated!"
