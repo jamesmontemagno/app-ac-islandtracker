81f24a7d39bb1b8df270bdf9fcad7504b1638b1d
James Montemagno <james.montemagno@gmail.com>
2020-04-11 21:20:59 -0700

    Fixup Name

--- TurnipTracker.Android/MainActivity.cs
+++ TurnipTracker.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TurnipTracker.Droid
 {
-    [Activity(Label = "TurnipTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Island Tracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
