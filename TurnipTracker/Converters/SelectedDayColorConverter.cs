1779265ce53f11603bfc56af7100a83521b193e8
James Montemagno <james.montemagno@gmail.com>
2020-04-12 20:22:48 -0700

    iOS Dark Mode Fixes & Rotation Fixes

---
+++ TurnipTracker/Converters/SelectedDayColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TurnipTracker.Converters
+{
+    public class SelectedDayColorConverter : IValueConverter
+    {
+        static Color acrylicColor;
+        static SelectedDayColorConverter()
+        {
+            acrylicColor = (Color)App.Current.Resources["AcrylicFrameBackgroundColor"];
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool val)
+                return val ? Color.LightPink : acrylicColor;
+
+            return acrylicColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
