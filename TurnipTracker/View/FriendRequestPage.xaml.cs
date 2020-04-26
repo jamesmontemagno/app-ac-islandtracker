aa0e929ae4b8b55712b82f779641d981e4c49e5b
James Montemagno <james.montemagno@gmail.com>
2020-04-26 08:33:11 -0700

    Add some new files

---
+++ TurnipTracker/View/FriendRequestPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TurnipTracker.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendRequestPage : ContentPage
+    {
+        public FriendRequestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
