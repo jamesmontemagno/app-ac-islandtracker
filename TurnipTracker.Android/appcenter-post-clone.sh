8ec7d3f03607e4c2b0554a1e7a9ac369089407e5
James Montemagno <james.montemagno@gmail.com>
2020-04-25 19:59:14 -0700

    Update Scripts

--- TurnipTracker.Android/appcenter-post-clone.sh
+++ TurnipTracker.Android/appcenter-post-clone.sh
@@ -7,6 +7,17 @@ sed -i '' "s/AC_ANDROID/$AC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker
 
 sed -i '' "s/AC_SYNC/$AC_SYNC/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
 
+sed -i '' "s/AC_BASEURL/$AC_BASEURL/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_GetFriendsKey/$AC_GetFriendsKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_GetFriendRequestsKey/$AC_GetFriendRequestsKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PostApproveFriendRequestKey/$AC_PostApproveFriendRequestKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_DeleteRemoveFriendKey/$AC_DeleteRemoveFriendKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PostSubmitFriendRequestKey/$AC_PostSubmitFriendRequestKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PutUpdateProfileKey/$AC_PutUpdateProfileKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PutUpdateTurnipPricesKey/$AC_PutUpdateTurnipPricesKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PostCreateProfileKey/$AC_PostCreateProfileKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+
+
 cat $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
 
 echo "Manifest updated!"
--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -11,6 +11,18 @@ sed -i '' "s/AC_SYNC/$AC_SYNC/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.x
 sed -i '' "s/APP_SECRET/$APP_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker.iOS/Info.plist
 
 
+sed -i '' "s/AC_BASEURL/$AC_BASEURL/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_GetFriendsKey/$AC_GetFriendsKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_GetFriendRequestsKey/$AC_GetFriendRequestsKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PostApproveFriendRequestKey/$AC_PostApproveFriendRequestKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_DeleteRemoveFriendKey/$AC_DeleteRemoveFriendKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PostSubmitFriendRequestKey/$AC_PostSubmitFriendRequestKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PutUpdateProfileKey/$AC_PutUpdateProfileKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PutUpdateTurnipPricesKey/$AC_PutUpdateTurnipPricesKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+sed -i '' "s/AC_PostCreateProfileKey/$AC_PostCreateProfileKey/g" $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
+
+
+
 cat $BUILD_REPOSITORY_LOCALPATH/TurnipTracker/App.xaml.cs
 
 cat $BUILD_REPOSITORY_LOCALPATH/TurnipTracker.iOS/Info.plist
