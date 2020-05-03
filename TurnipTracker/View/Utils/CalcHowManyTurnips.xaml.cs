a10a89245b2d129cb328cc5d0c2be0c65f7d8976
James Montemagno <james.montemagno@gmail.com>
2020-05-03 09:48:55 -0700

    Add Turnip Calculates

---
+++ TurnipTracker/View/Utils/CalcHowManyTurnips.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TurnipTracker.View.Utils
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcHowManyTurnips : ContentPage
+    {
+        public CalcHowManyTurnips()
+        {
+            InitializeComponent();
+        }
+
+        private void ValueChanged(object sender, Syncfusion.SfNumericTextBox.XForms.ValueEventArgs e)
+        {
+            if (EntryTurnipPrice.Value == null ||
+                 EntryBellsToSpend.Value == null)
+                return;
+
+            var turnip = (double)EntryTurnipPrice.Value;
+            var bells = (double)EntryBellsToSpend.Value;
+
+
+            if (turnip == 0 || bells == 0)
+                return;
+
+            var total = (int)(bells / (turnip * 10));
+            LabelCalculation.Text = $"You can buy {total.ToString("n0")} bundles ({total/10} slots)";
+        }
+    }
+}
