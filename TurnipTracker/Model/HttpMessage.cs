0b8a18e1cbdefe292981064dd9b3a1ecc98eb412
James Montemagno <james.montemagno@gmail.com>
2020-05-04 16:21:01 -0700

    More updates for httpmessages

---
+++ TurnipTracker/Model/HttpMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurnipTracker.Model
+{
+    public class HttpMessage
+    {
+        public string Message { get; set; }
+    }
+}
