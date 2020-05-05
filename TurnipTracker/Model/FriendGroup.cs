65675fd5edfc241b513ea473206999570950b057
James Montemagno <james.montemagno@gmail.com>
2020-05-05 12:07:03 -0700

    Add grouping for friends list :)

---
+++ TurnipTracker/Model/FriendGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using MvvmHelpers;
+using TurnipTracker.Shared;
+
+namespace TurnipTracker.Model
+{
+    public class FriendGroup : ObservableRangeCollection<FriendStatus>
+    {
+        public string Key { get; }
+        /// <summary>
+		/// Returns list of items in the grouping.
+		/// </summary>
+		public new IList<FriendStatus> Items => base.Items;
+        public FriendGroup(string key, IEnumerable<FriendStatus> items)
+        {
+            Key = key;
+            AddRange(items);
+        }
+    }
+}
